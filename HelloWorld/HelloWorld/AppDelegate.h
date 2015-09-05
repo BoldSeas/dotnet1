50d8ce20c64ba9527df297cc902e7d858779f12b
zhangxiaopeng1204 <510631648@qq.com>
2015-09-05 13:12:48 +0800

    init project

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 张敬朋 on 15/9/5.
+//  Copyright © 2015年 YFB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
